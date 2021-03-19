b6c8924b03c3cb2e8fbf53fbdd98aeac62530c40
JackLin7213 <jackharper775@gmail.com>
2021-03-19 15:41:03 +0800

    commit2.0

--- products.py
+++ products.py
@@ -11,8 +11,9 @@ while True:
     #快寫法 p = [name,price]
     products.append(p)
     #快寫法 products.append([name, price])
-    
-
 print(products)
 
-products[0][0]
+for p in products:
+    print(p[0], '的價格是', p[1])
+
+products[0][0] #大清單中的第一個,小清單中的第一個
